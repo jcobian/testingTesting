bb33a2a950da71c679f10decb11d57a475c608c4
jcobian <jcobian@civisanalytics.com>
2015-10-26 16:16:00 -0500

    add check for db env vars

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import os
 print("My awesome git repo")
+db_user = os.environ.get('DATABASE_USER')
+db_password = os.environ.get('DATABASE_PASSWORD')
+if db_user and db_password:
+    print "DB User: " + db_user
+
+
