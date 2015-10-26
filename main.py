6464f365ee14178471a9deaebe1731ddf297fad0
jcobian <jcobian@civisanalytics.com>
2015-10-26 16:37:23 -0500

    python 3 parents on print

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ print("My awesome git repo")
 db_user = os.environ.get('DATABASE_USER')
 db_password = os.environ.get('DATABASE_PASSWORD')
 if db_user and db_password:
-    print "DB User: " + db_user
+    print("DB User: " + db_user)
 
 
