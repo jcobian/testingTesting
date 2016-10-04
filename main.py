73e131750cf7836aecebd9d6f69598c6ee6a62a5
jcobian <jcobian@civisanalytics.com>
2016-10-04 11:21:21 -0500

    list me

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import civis
-from civis.base import Endpoint
-Endpoint._base_url = 'https://api-staging.civisanalytics.com/'
 client = civis.APIClient()
-print(client.users.list_me()['name'])
+# HTTP  GET /users/me
+client.users.list_me()
