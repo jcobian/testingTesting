574880e57a3d5691bca39c6a829267ce569e288a
jcobian <jcobian@civisanalytics.com>
2017-01-10 18:14:02 -0600

    run outputs test

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import os
 import civis
+from civis.base import Endpoint
+Endpoint._base_url = 'https://api-staging.civisanalytics.com/'
+from civis.io import file_to_civis
 client = civis.APIClient()
-# HTTP  GET /users/me
-print(client.users.list_me())
+file_name = "output file with awesome things in it"
+file_id = file_to_civis("this file is so cool yay", file_name)
+job_id = os.environ.get('CIVIS_JOB_ID')
+run_id = os.environ.get('CIVIS_RUN_ID')
+# create file output
+client.scripts.post_python3_runs_outputs(job_id, run_id, 'File', file_id)
+# create table output
+table_id = 2127734
+client.scripts.post_python3_runs_outputs(job_id, run_id, 'Table', table_id)
+# create report output
+report_id = 26876
+client.scripts.post_python3_runs_outputs(job_id, run_id, 'Report', report_id)
+
+
+
